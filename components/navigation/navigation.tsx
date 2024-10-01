671220781d340a4f99a97a4406ae1b5f19c7828b
DuetoPark <inhye.park.dev@gmail.com>
2024-10-01 10:53:39 +0900

    💄 [style] Navigation 구현

---
+++ components/navigation/navigation.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./navigation.module.css";
+
+const Navigation = () => {
+  const path = usePathname();
+
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li className={path === "/" ? styles.active : ""}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={path === "/about-us" ? styles.active : ""}>
+          <Link href="/about-us">About Us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
