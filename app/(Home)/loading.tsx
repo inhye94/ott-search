5aae011e177e5004883783ddff22d6dd5359e83b
DuetoPark <inhye.park.dev@gmail.com>
2024-10-10 01:08:41 +0900

    💄 [style] page loading 구현

--- app/(Home)/loading.tsx
+++ app/(Home)/loading.tsx
@@ -1,5 +1,14 @@
+import { FadeLoader } from "react-spinners";
+
+import styles from "./loading.module.css";
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.loading}>
+      <FadeLoader color="aquamarine" height={12} />
+      <p>페이지를 로딩중입니다!</p>
+    </div>
+  );
 };
 
 export default Loading;
