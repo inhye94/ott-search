eeb5b6c1465954d3ffdcfd024c278669c989aade
DuetoPark <inhye.park.dev@gmail.com>
2024-10-18 01:04:07 +0900

    🏷️ [type] 아차..! props type - FlipCard

--- src/components/flip-card/flip-card.tsx
+++ src/components/flip-card/flip-card.tsx
@@ -1,19 +1,21 @@
 import classNames from "classnames";
 import styles from "./flip-card.module.css";
 
+export interface FlipCardPropsType {
+  path: string;
+  title: string;
+  overview: string;
+  variant?: "square" | "normal";
+  round?: boolean;
+}
+
 const FlipCard = ({
   path,
   title,
   overview,
   variant,
   round,
-}: {
-  path: string;
-  title: string;
-  overview: string;
-  variant?: "square" | "normal";
-  round?: boolean;
-}) => {
+}: FlipCardPropsType) => {
   return (
     <div
       className={classNames(
