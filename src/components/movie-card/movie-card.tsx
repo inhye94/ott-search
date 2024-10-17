a12eac7764d153bf915919eccbaa1599a88988f4
DuetoPark <inhye.park.dev@gmail.com>
2024-10-18 00:51:45 +0900

    🐛 [error & refactoring] props type 에러 대응

--- src/components/movie-card/movie-card.tsx
+++ src/components/movie-card/movie-card.tsx
@@ -4,29 +4,31 @@ import Link from "next/link";
 
 import styles from "./movie-card.module.css";
 import { useRouter } from "next/navigation";
+import { useCallback, useRef } from "react";
 
 interface MovieCardPropsType {
-  id: string;
+  id: number;
   poster_path: string;
   title: string;
 }
 
 const MovieCard = ({ id, poster_path, title }: MovieCardPropsType) => {
-  const detailLink = `/${id}/movie`;
+  const detailLink = useRef<string>(`/${id}/movie`);
 
   const router = useRouter();
-  const pushRouter = () => {
-    router.push(detailLink);
-  };
+
+  const pushDetailPageToHistory = useCallback(() => {
+    router.push(detailLink.current);
+  }, []);
 
   return (
     <article className={styles.card}>
       <img
         src={poster_path}
         alt={`${poster_path}의 포스터`}
-        onClick={pushRouter}
+        onClick={pushDetailPageToHistory}
       />
-      <Link prefetch href={detailLink}>
+      <Link prefetch href={detailLink.current}>
         {title}
       </Link>
     </article>
