4f57b69e4a3e020b698b83616ca669edd1950347
DuetoPark <inhye.park.dev@gmail.com>
2024-11-21 11:55:08 +0900

    🎨 [markup] 마크업 변경 - about-us

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -16,32 +16,53 @@ const AboutUsPage = () => {
         <FlipCard
           path="https://avatars.githubusercontent.com/u/69448900?v=4"
           title="profile"
-          overview="1997년 4살 박인혜"
+          overview="4살 박인혜"
           variant="square"
           round
         />
       </div>
 
       <div className={styles.github}>
-        <Tag tag="link" href="https://github.com/DuetoPark">
-          @DuetoPark &rarr;
+        <Tag tag="link" href="https://github.com/inhye94">
+          @inhye94 &rarr;
         </Tag>
       </div>
 
       <p className={styles.desc}>
-        due to: ~으로 인해
-        <br />
-        숙어 뜻이 저의 이름과 비슷해서 dueto park가 되었습니다.
-        <br />
-        제 친구 서우는 west cow 이며, 소희는 little joy, 수빈이는 desert 입니다.
-        <br />
-        <br />
         어렸을 때 웃는 얼굴이 꽤나 맥도날드 마스코트와 비슷하게 생겼네요.
         <br />
-        고양이를 좋아하지만 잘 키울 자신이 없어서 사이버 집사로 만족하고
-        있습니다.
         <br />
-        강아지도 좋아합니다.
+        얼마전에 깃허브 네임을 변경했습니다.
+        <br />
+        (Dueto Park &gt;&gt; inhye94)
+        <br />
+        영어 이름이 부끄러워서요.
+        <br />
+        <br />
+        날씨가 좋은 날 강남의 한 카페에서
+        <br />
+        친구들과 영어이름 짓기를 시작되었습니다.
+        <br />
+        제 이름이 '인혜'인데, due to라는 숙어의 뜻이 '~으로 인해'더라구요.
+        <br />
+        발음이 비슷하다는 이유로 dueto park가 되었습니다.
+        <br />
+        TMI이지만 제 친구 서우는 west cow, 소희는 little joy, 수빈이는
+        desert입니다.
+        <br />
+        짧은 가방끈이 참 애석하네요.
+        <br />
+        <br />
+        이런들 어떠하며, 저런들 어떠합니까
+        <br />
+        즐거우면 된거라 생각합니다.
+        <br />
+        덕분에 저와 친구들은 좋은 추억을 쌓았으니까요.
+        <br />
+        <br />
+        엉뚱한 이유일지라도 과정이 재미있는 개발을 하고 싶습니다.
+        <br />
+        매우 소박한 저의 개발 철학입니다.
       </p>
     </Layout>
   );
